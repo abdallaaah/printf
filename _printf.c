a4d3c3aef4eb4fde0af01772ea9d847be9880cf3
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-06-25 13:23:30 -0700

    _printf and _putchar from testing phase to production

---
+++ _printf.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * _printf - the main control function to control every thing
+ * @format: the first arg in va_list
+ * Return: number of printed
+ */
+int _printf(char *format, ...)
+{
+va_list args;
+int i = 0;
+unsigned int character;
+char *string;
+int z = 0;
+va_start(args, format);
+while (format[i] != '\0')
+{
+if (format[i] == '%')
+{
+i++;
+switch (format[i])
+{
+case 'c':
+character = va_arg(args, int);
+_putchar(character);
+break;
+case 's':
+string = va_arg(args, char *);
+while (string[z] != '\0')
+{
+_putchar(string[z]);
+z++;
+}
+z = 0;
+break;
+}
+}
+else
+{
+_putchar(format[i]);
+}
+i++;
+}
+va_end(args);
+return (i);
+}
