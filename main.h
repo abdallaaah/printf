987d1a73f59a737b474f72c36891b525f26f5fbe
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-06-25 13:44:56 -0700

    check

--- main.h
+++ main.h
@@ -4,16 +4,6 @@
 #include <string.h>
 #include <stdarg.h>
 #include <unistd.h>
-/**
- * _putchar - function use write to stout
- * @c: the char we will print
- * Return: numbers printed
- */
 void _putchar(char c);
-/**
- * _printf - the main control function to control every thing
- * @format: the first arg in va_list
- * Return: number of printed
- */
 int _printf(char *format, ...);
-#endif
+eendif
