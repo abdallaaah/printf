4c9682c8b1a96e9ab9f8cdbe53d6a151a8a45780
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-06-25 13:35:19 -0700

    fix the compailation problem

--- _printf.c
+++ _printf.c
@@ -11,6 +11,7 @@ int i = 0;
 unsigned int character;
 char *string;
 int z = 0;
+int num = 0;
 va_start(args, format);
 while (format[i] != '\0')
 {
@@ -22,12 +23,14 @@ switch (format[i])
 case 'c':
 character = va_arg(args, int);
 _putchar(character);
+num++;
 break;
 case 's':
 string = va_arg(args, char *);
 while (string[z] != '\0')
 {
 _putchar(string[z]);
+num++;
 z++;
 }
 z = 0;
@@ -41,5 +44,6 @@ _putchar(format[i]);
 i++;
 }
 va_end(args);
-return (i);
+printf("the number is %d\n", num);
+return (num);
 }
