af4cb46379059b643498db50cbac1a40f2f18ef4
hazemghaly <hazemghaly30@gmail.com>
2023-06-26 13:29:03 -0700

    printf commit with print number d

--- main.c
+++ main.c
@@ -9,11 +9,14 @@
  */
 int main(void)
 {
+int y  = 25;
 char *str = "I am a string !";
 char *str2 = "I am a string2 !";
 printf("Character:[%c]hello[%c]\n", 'H', 'h');
 _printf("Character:[%c]hello[%c]\n", 'H', 'h');
 printf("String:[%s][%s]\n", str, str2);
 _printf("String:[%s][%s]\n", str, str2);
+printf("length:[%d]\n", y);
+_printf("length:[%d]\n", y);
 return (0);
 }
