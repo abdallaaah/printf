f6c5624f7570771b44a02d46b56fb7b4f55efa0e
hazemghaly <hazemghaly30@gmail.com>
2023-06-26 13:14:06 -0700

    printf commit with print number d

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@ int _printf(char *format, ...)
 va_list args;
 unsigned int character;
 char *string;
-int z = 0, num = 0, i = 0;
+int y, z = 0, num = 0, i = 0;
 va_start(args, format);
 while (format[i] != '\0')
 {
@@ -32,6 +32,10 @@ _putchar(string[z]);
 z++;
 }
 z = 0;
+break;
+case 'd':
+y = va_arg(args, int);
+(y == (int)y) ?	_printint(y) : _putchar('y');
 break;
 default:
 num++;
