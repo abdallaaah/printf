377ec4cefb6867123d14a57fc77f03b073b229d9
abdallaaah <eng.abdallaeltawil@gmail.com>
2023-06-30 13:22:34 -0700

    check lenghts

--- _printf.c
+++ _printf.c
@@ -53,5 +53,5 @@ num++;
 _putchar(format[i]);
 } i++;
 } va_end(args);
-return (num + 2);
+return (num);
 }
--- main.c
+++ main.c
@@ -9,7 +9,9 @@
  */
 int main(void)
 {
-int check = 0;
+
+int check = 0, check2= 0;
+/*
 char *str = "I am a string !";
 char *str2 = "I am a string2 !";
 printf("Character:[%c]hello[%c]\n", 'H', 'h');
@@ -21,6 +23,10 @@ _printf("length:[%d]\n", 899);
 check = printf("length:[%i]\n", 0x2a);
 printf("check from orignal:[%d]\n",check);
 check =_printf("length:[%i]\n", 0x2a);
-printf("check from fake:[%d]\n",check);
+printf("check from fake:[%d]\n",check);*/
+check = printf("Let's print a simple sentence.\n");
+printf("the origianl check :%d\n",check);
+check2 = _printf("Let's print a simple sentence.\n");
+printf("the fake check :%d\n",check2);
 return (0);
 }
